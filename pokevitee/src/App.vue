81b85486a6f4bc0da823b96ea4ee523b24b364cf
Eduardo Matheus de Menezes Souto <eduardomatheusuesc@gmail.com>
2023-05-15 10:10:23 -0300

    feat: First Commit

---
+++ pokevitee/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+  import {ref} from 'vue'
+
+  let num = ref(0)
+</script>
+
+<template>
+  <div>
+    <button @click="num++">
+      {{ num }}
+    </button>
+  </div>
+</template>
+
+<style>
+  
+</style>
